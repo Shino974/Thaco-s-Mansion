5e433c32f9a73c5f5eac4fc701076dcb7160a232
Shino974 <theo.turpin@epitech.eu>
2023-12-02 00:12:33 +0900

    feat(Cursor): hide cursor
    Hide cursor on game

---
+++ Assets/Scripts/HideCursorScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursorScript : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+}
